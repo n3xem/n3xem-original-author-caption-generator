d5999f7eb674243c482985b628d8f1f4f8b0a6ac
yukyan <yukyanjpn@gmail.com>
2025-04-16 09:08:59 +0900

    OGPを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,28 @@ const notoSansJP = Noto_Sans_JP({
 
 export const metadata: Metadata = {
     title: "原作者テロップジェネレーター",
-    description: "画像に原作者クレジットを追加するツール",
+    description: "画像に原作者クレジットを追加するツールです。画像をアップロードするだけで画像を生成でき、細かい調整も可能です。",
+    openGraph: {
+        title: "原作者テロップジェネレーター",
+        description: "画像に原作者クレジットを追加するツールです。画像をアップロードするだけで画像を生成でき、細かい調整も可能です。",
+        images: [
+            {
+                url: "/ogp.png",
+                width: 1200,
+                height: 630,
+                alt: "原作者テロップジェネレーター",
+            },
+        ],
+        locale: "ja_JP",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "原作者テロップジェネレーター",
+        description: "画像に原作者クレジットを追加するツールです。画像をアップロードするだけで画像を生成でき、細かい調整も可能です。",
+        images: ["/ogp.png"],
+        creator: "@yukyan_p",
+    },
 };
 
 export default function RootLayout({
